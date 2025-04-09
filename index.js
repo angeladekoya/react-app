32740735a3b5b848ed99d761c8b74e47b3ec5e5a
angeladekoya <angeladekoya10@gmail.com>
2025-04-09 12:38:32 +0100

    add node.js

---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('{ "response": "Hello welcome to the cloud computing" }');
+});
+
+app.get('/will', function (req, res) {
+    res.send('{ "response": "Hello Angel of God welcome to my website" }');
+});
+app.get('/ready', function (req, res) {
+    res.send('{ "response": " hurray!, Its working fine!" }');
+});
+app.get('/ ready', function(req, res) {
+
+    res.send('{ "response": "hurray!, Its working fine!" }');
+});
+
+app.listen(process.env.PORT || 3500);
+console.log("server running---and connected")
+module.exports = app;
